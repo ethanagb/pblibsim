71542505200552e0548521e10c6cbc227da17587
Ethan A. G. Baker <ethanagbaker@gmail.com>
2016-05-20 15:55:48 -0400

    eliminated parameter, still needs multiprocessing support

--- v1/pythonGetFastas.py
+++ v1/pythonGetFastas.py
@@ -1,20 +1,9 @@
+import getopt, multiprocessing
+import pybedtools
+
+threads = multiprocessing.cpu_count() #default
+bedNameFile = 'bedNames.txt'
 def getFastas(argv):
-	import pybedtools
-	threads = 1 #default
-	try:
-        opts, args = getopt.getopt(argv,hi:o:t:,["infile=","outfile=","threads="])
-    except getopt.GetoptError:
-        print("Usage: python getFastas.py --infile </path/to/infile.txt> --outfile </path/to/outfile.bed> -t <number of threads>")
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print("Usage: python getFastas.py --infile </path/to/infile.txt> --outfile </path/to/outfile (name root)> -t <number of threads>")
-        elif opt ==("-o","--outfile"):
-            outfileName = arg
-        elif opt ==("-i", "--infile"):
-            bedNameFile = arg
-        elif opt ==("-t", "--threads"):
-            threads = arg
 	with open(bedNameFile, 'r') as infile:
 		bedfileslist = infile.readlines().strip()
 	infile.close()
@@ -25,6 +14,6 @@ def getFastas(argv):
 		b = a.save_seqs(outfileName + "_" + str(outfileCount) + ".fa")
 		assert open(b.fn).read() == open(a.fn).read()
 		outfileCount += 1
-
+		
 if __name__ == "__main__":
     getFastas(sys.argv[1:])
